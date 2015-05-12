d1ef555634ac9ce6e1d6f8379a0ce8e72d8b5c16
Josef Stribny <jstribny@redhat.com>
2015-05-12 15:37:07 +0200

    test returns the right exit code, removing

--- lib/vagrant-atomic/cap/docker.rb
+++ lib/vagrant-atomic/cap/docker.rb
@@ -3,7 +3,6 @@ module VagrantPlugins
     module Cap
       module Docker
         def self.docker_daemon_running(machine)
-          #not sure this is the best test, seems to always pass
           machine.communicate.test("test -S /run/docker.sock")
         end
       end
