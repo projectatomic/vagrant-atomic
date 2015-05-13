5e96a7e1acfe13a144ca6cd0a523360af060191b
Josef Stribny <jstribny@redhat.com>
2015-05-13 08:55:52 +0200

    Properly recognize Atomic guest

--- lib/vagrant-atomic/guest.rb
+++ lib/vagrant-atomic/guest.rb
@@ -4,13 +4,7 @@ module VagrantPlugins
   module GuestAtomic
     class Guest < Vagrant.plugin("2", :guest)
       def detect?(machine)
-	# For now we use this non 100% bullet proof solution.
-	# Bugs issued to include word atomic in upstream releases
-	# Centos: http://bugs.centos.org/view.php?id=8288
-	# Fedora: https://bugzilla.redhat.com/show_bug.cgi?id=1200122
-	#
-	# machine.communicate.test("cat /etc/os-release | grep Atomic")
-        machine.communicate.test("which rpm-ostree")
+        machine.communicate.test("grep 'ostree=' /proc/cmdline")
       end
     end
   end
